ba50232f5fa8b70e92cbd497d3a2d7263fea35ae
WJ <WJ@Kees-Air.lan>
2020-04-21 13:17:11 +0800

    added spritesheet(fox)

--- main.js
+++ main.js
@@ -8,17 +8,22 @@ var GameState = {
 
     },
     create :function(){
-
         this.background = this.game.add.sprite(0,0,'background');
 
+        this.foxi = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,"foxi");
+        this.foxi.anchor.setTo(0.5);
 
     },
     preload:function(){
         this.load.image('background','images/vortex.png')
+        this.load.spritesheet("foxi","images/foxi.png",19,16,8,0,0);
+        this.load.spritesheet("foxr","images/foxr.png",17,16,9,0,0);
+
+
     }
 
 };
-var game = new Phaser.Game(1000,1000,Phaser.AUTO);
+var game = new Phaser.Game(800,600,Phaser.AUTO);
 
 
 game.state.add('GameState',GameState)
